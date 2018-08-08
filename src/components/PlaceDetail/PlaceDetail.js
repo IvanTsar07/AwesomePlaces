27d3ad3f29b350b01149b20da90abb7f3983317e
IvanTsar07 <ivan.tsar07@gmail.com>
2018-08-08 17:48:50 +0300

    Material Icons

--- src/components/PlaceDetail/PlaceDetail.js
+++ src/components/PlaceDetail/PlaceDetail.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { View, Text, StyleSheet, Image, Modal, Button } from 'react-native';
+import { View, Text, StyleSheet, Image, Modal, Button, TouchableOpacity } from 'react-native';
 
+import Icon from "react-native-vector-icons/Ionicons"
 
 const placeDetail = props => {
     let modalContent = null;
@@ -18,9 +19,18 @@ const placeDetail = props => {
         <Modal onRequestClose={props.onModalClosed} visible={props.selectedPlace !== null} animationType="slide">
             <View style={styles.modalContainer} >
                 {modalContent}
-                <View>
-                    <Button title="Delete" color="red" onPress={props.onItemDeleted}></Button>
-                    <Button title="Close" onPress={props.onModalClosed}></Button>
+                <View style={styles.buttonContainer} >
+                    <TouchableOpacity onPress={props.onItemDeleted} >
+                        <View style={styles.deleteButton} >
+                            <Icon size={30} name="ios-trash" color="red" />
+                        </View>
+                    </TouchableOpacity>
+                   
+                    <TouchableOpacity onPress={props.onModalClosed} >
+                        <View style={styles.deleteButton} >
+                            <Icon size={40} name="ios-close" color="blue" />
+                        </View>
+                    </TouchableOpacity>
                 </View>
             </View>
         </Modal>
@@ -40,6 +50,22 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         textAlign: "center",
         fontSize: 28
+    },
+    buttonContainer: {
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        flexDirection: "row",
+        marginTop: 20
+    },
+    deleteButton: {
+        alignItems: 'center',
+        width: 50,
+        heigt: 50
+    },
+    closeButton: {
+        alignItems: 'center',
+        width: 50,
+        heigt: 50
     }
 });
 
